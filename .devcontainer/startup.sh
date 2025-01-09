968290346d7aa5066818e4e75b1b1554b1853fa9
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-09 20:44:46 +0400

    devcontainer

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uv venv
+uv venv --python 3.12
 uv pip install --no-cache-dir jupyterlab
 uv pip install --no-cache-dir -r requirements.txt
